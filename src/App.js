117da7caccbab8b9111d973defe3321a627c8b3b
anselbrandt <ansel@anselbrandt.com>
2020-08-10 00:21:42 -0400

    Make initial viewstate responsive

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import styles from "./App.module.css";
 import Map from "./Map";
 import { FlyToInterpolator } from "react-map-gl";
 import Legend from "./Legend";
-
-const locations = {
-  montreal: {
-    longitude: -73.7,
-    latitude: 45.55,
-    zoom: 11,
-    pitch: 0,
-    bearing: 0,
-  },
-};
+import useGetViewport from "./useGetViewport";
 
 function App() {
+  const { width } = useGetViewport();
+  const locations = {
+    Montreal: {
+      longitude: width < 800 ? -73.65 : -73.7,
+      latitude: 45.55,
+      zoom: width < 800 ? 10 : 11,
+      pitch: 0,
+      bearing: 0,
+    },
+  };
   const [target] = useState(400000);
   const [range] = useState([0.05, 0.15, 0.3]);
-  const [viewState, setViewState] = useState(locations.montreal);
+  const [viewState, setViewState] = useState(locations.Montreal);
   const handleChangeViewState = ({ viewState }) => setViewState(viewState);
   const handleFlyTo = (destination) => {
     setViewState({
--- src/locations.js
+++
@@ -1,7 +0,0 @@
-export const Montreal = {
-  longitude: -73.7,
-  latitude: 45.55,
-  zoom: 11,
-  pitch: 0,
-  bearing: 0,
-};
