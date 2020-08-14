b888828baf19eb5aa074d74b7324033007fd960c
anselbrandt <ansel@anselbrandt.com>
2020-08-14 09:58:36 -0400

    Fix useGetBins bug

--- src/useGetBins.js
+++ src/useGetBins.js
@@ -4,7 +4,7 @@ export default function useGetBins(target, range) {
       ...range.sort((a, b) => b - a).map((value) => 1 + value),
 
       1,
-      ...range.sort((a, b) => b - a).map((value) => 1 - value),
+      ...range.sort((a, b) => a - b).map((value) => 1 - value),
       0,
     ],
   ].map((value) => value * target);
